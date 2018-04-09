dddaa0ffa967389d433cb4342979c491c3909676
Fiona Lapham <fionadlapham@gmail.com>
2018-04-09 14:22:43 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,10 @@
-# require libraries/modules here
+require 'Nokogiri'# require libraries/modules here
+
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
+  
   # write your code here
 end
