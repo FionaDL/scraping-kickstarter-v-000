1d47909322f2b68e1919ab01deb44d0ccd214e83
Fiona Lapham <fionadlapham@gmail.com>
2018-04-09 14:34:22 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,13 +1,16 @@
 require 'nokogiri'# require libraries/modules here
 require 'pry'
 
+# projects: kickstarter.css("li.project.grid_4")
+
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
 
+binding.pry
   # write your code here
 end
 
 create_project_hash
-binding.pry
+
